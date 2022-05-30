a29e59059e225176e3adb3e1242be3759c08cb51
stickman89 <csstikman89@aol.com>
2022-05-30 18:17:57 +0100

    remove lineage-overlay
    ...

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ $(call inherit-product, vendor/xiaomi/lithium/lithium-vendor.mk)
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # Audio
 PRODUCT_COPY_FILES += \
